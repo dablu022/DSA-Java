7bb921f2c0fae99a13dc3e85a333e4b1c1bd055b
DABLU KUMAR <yash278219@gmail.com>
2026-04-07 23:36:16 +0530

    Time: 2 ms (100.00%) | Memory: 46.5 MB (70.57%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,21 @@
+class Solution{
+    public int romanToInt(String s){
+        int ans = 0;
+        int num = 0;
+        for(int i = s.length() - 1; i >= 0; i--){
+            char ch = s.charAt(i);
+            
+            if(ch == 'I') num = 1;
+            else if(ch == 'V') num = 5;
+            else if(ch == 'X') num = 10;
+            else if(ch == 'L') num = 50;
+            else if(ch == 'C') num = 100;
+            else if(ch == 'D') num = 500;
+            else if(ch == 'M') num = 1000;
+
+            if(4 * num < ans) ans -= num;
+            else ans += num;
+        }
+        return ans;
+    }
+}
