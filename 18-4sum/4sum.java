9b3428005656d630bc8d6fc2a87d1cce80201a6e
DABLU KUMAR <yash278219@gmail.com>
2026-07-22 16:03:25 +0530

    Time: 17 ms (93.25%) | Memory: 45.8 MB (63.16%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -7,22 +7,17 @@ class Solution {
         int n = nums.length;
 
         for(int i = 0; i < n - 3; i++) {
-
             if(i > 0 && nums[i] == nums[i - 1])
                 continue;
-
             for(int j = i + 1; j < n - 2; j++) {
-
                 if(j > i + 1 && nums[j] == nums[j - 1])
                     continue;
 
                 int k = j + 1;
                 int l = n - 1;
-
                 while(k < l) {
 
                     long sum = (long)nums[i] + nums[j] + nums[k] + nums[l];
-
                     if(sum < target) {
                         k++;
                     }
@@ -35,7 +30,6 @@ class Solution {
 
                         k++;
                         l--;
-
                         while(k < l && nums[k] == nums[k - 1])
                             k++;
 
